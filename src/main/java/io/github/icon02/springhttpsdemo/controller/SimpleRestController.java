1a1d0dd9f7524f05df3e72ac74f2a5a69e3f8ec8
Nico <nico.layr@gmx.at>
2021-09-13 19:59:59 +0200

    updated controller

--- src/main/java/io/github/icon02/springhttpsdemo/controller/SimpleRestController.java
+++ src/main/java/io/github/icon02/springhttpsdemo/controller/SimpleRestController.java
@@ -6,11 +6,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.github.icon02.springhttpsdemo.payload.Wrapper;
 
+import java.io.File;
+
 @RestController
 public class SimpleRestController {
+
+    private static final double GB = 1024d * 1024d * 1024d;
 
     @GetMapping("/test")
     public ResponseEntity<Wrapper<String>> test() {
         return ResponseEntity.ok(new Wrapper<>("Test"));
+    }
+
+    @GetMapping("/total-space")
+    public ResponseEntity<Wrapper<Double>> getTotalSpace() {
+        File file = new File(".");
+        return ResponseEntity.ok(new Wrapper<>((double) file.getTotalSpace() / GB));
+    }
+
+    @GetMapping("/free-space")
+    public ResponseEntity<Wrapper<Double>> getFreeSpace() {
+        File file = new File(".");
+        return ResponseEntity.ok(new Wrapper<>((double) file.getFreeSpace() / GB));
+    }
+
+    @GetMapping("/usable-space")
+    public ResponseEntity<Wrapper<Double>> getUsableSpace() {
+        File file = new File(".");
+        return ResponseEntity.ok(new Wrapper<>((double) file.getUsableSpace() / GB));
     }
 }
